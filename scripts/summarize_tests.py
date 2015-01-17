1475a2e854c84688c406bb23997b63528ce94116
Gavin Price <gaprice@lbl.gov>
2015-01-16 16:46:05 -0800

    Calculate test duration mean & stddev

--- scripts/summarize_tests.py
+++ scripts/summarize_tests.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 from collections import defaultdict
 from urllib2 import urlopen
 import json
+import numpy
 
 START_JOB = 238
 STOP_JOB = 260
@@ -21,6 +22,7 @@ def collect_test_data():
     url_prefix = JENKINS_URL + '/job/' + TEST_SUITE_NAME + '/'
     res = defaultdict(lambda: defaultdict(list))
     testcount = defaultdict(int)
+    timings = defaultdict(list)
     for job in xrange(START_JOB, STOP_JOB + 1):
         url = url_prefix + str(job) + '/api/json'
         j = json.loads(urlopen(url).read())
@@ -31,22 +33,29 @@ def collect_test_data():
         for test in j['suites'][0]['cases']:
             name = test['name']
             error = test['errorDetails']
+            duration = float(test['duration'])
             testcount[name] += 1
             if error:
                 res[name][error].append(job)
-    return testcount, res
+            else:
+                timings[name].append(duration)
+    return testcount, res, timings
 
 
-def print_test_summary_data(testcount, res):
+def print_test_summary_data(testcount, res, timings):
     total = 0
     fails = 0
-    print('Test\tTotal\tFails')
+    print('Test\tTotal\tFails\tMean (s)\tStddev (s)\tN')
     for test in testcount:
         test_fails = 0
         if test in res:
             for error in res[test]:
                 test_fails += len(res[test][error])
-        print(test + '\t' + str(testcount[test]) + '\t' + str(test_fails))
+        mean = numpy.mean(timings[test])
+        stddev = numpy.std(timings[test], ddof=0)
+        N = testcount[test] - test_fails
+        print(test + '\t' + str(testcount[test]) + '\t' + str(test_fails) +
+              '\t' + str(mean) + '\t' + str(stddev) + '\t' + str(N))
         total += testcount[test]
         fails += test_fails
     print('TTL\t' + str(total) + '\t' + str(fails))
@@ -62,9 +71,9 @@ def print_test_errors(res):
 
 
 def main():
-    testcount, res = collect_test_data()
+    testcount, res, timings = collect_test_data()
     print('Results for jobs from ' + str(START_JOB) + ' to ' + str(STOP_JOB))
-    print_test_summary_data(testcount, res)
+    print_test_summary_data(testcount, res, timings)
     print()
     print_test_errors(res)
 
